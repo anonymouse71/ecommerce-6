9b16846516cd7ddcf9686255794550ed0edf8d0f
Limited7247 <lt-dat@outlook.com>
2016-03-31 23:17:33 +0700

    Added Check Order
    Added Check Order

--- Templates/topmenu.php
+++ Templates/topmenu.php
@@ -5,6 +5,7 @@
             <li><a href="products.php">Sản phẩm</a></li>
             <li><a href="contact.php">Liên hệ</a></li>
             <li><a href="search.php">Tìm kiếm</a></li>
+            <li><a href="checkorder.php">Kiểm tra đơn hàng</a></li>
         </ul>
     </nav>
 </div>
